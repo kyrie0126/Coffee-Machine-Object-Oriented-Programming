e26e848215422a1b09dc7e91dbea69d60a0e26b7
kyrie0126 <115884650+kyrie0126@users.noreply.github.com>
2022-10-27 09:22:22 -0400

    initial upload of base files

---
+++ main.py
@@ -0,0 +1,14 @@
+from menu import Menu, MenuItem
+from coffee_maker import CoffeeMaker
+from money_machine import MoneyMachine
+
+print("Welcome to the coffee maker")
+
+menu = Menu()
+print(menu)
+
+
+
+
+
+
