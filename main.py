f0444f1756df5ac240cc97823ad15a01d915a492
kyrie0126 <kylewong00@gmail.com>
2022-10-30 19:50:32 -0400

    initial commit

--- main.py
+++ main.py
@@ -1,12 +1,35 @@
+import coffee_maker
 from menu import Menu, MenuItem
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
 
-print("Welcome to the coffee maker")
-
+# initialize objects
 menu = Menu()
-print(menu)
+machine = CoffeeMaker()
+money = MoneyMachine()
 
+# things start happening
+
+
+def coffee_creation():
+    drink = str(input(f"Choose from the following drinks '{menu.get_items()}'\n")).lower()
+
+    if drink == "report":
+        machine.report()
+    elif drink == "off":
+        return
+
+    drink_info = menu.find_drink(drink)
+    if machine.is_resource_sufficient(drink_info):
+        money.make_payment(drink_info.cost)
+        machine.make_coffee(drink_info)
+        print("\n")
+        coffee_creation()
+    else:
+        return
+
+
+coffee_creation()
 
 
 
